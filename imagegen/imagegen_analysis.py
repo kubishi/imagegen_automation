e697df492707eb2c0ed6727fb39db1fb975a45f4
kathan11 <kspathak@icloud.com>
2025-02-06 18:08:43 -0800

    adds minor changes in pipeline and analysis

--- imagegen/imagegen_analysis.py
+++ imagegen/imagegen_analysis.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 thisdir = pathlib.Path(__file__).parent.resolve()
 
+
 def main():
     # Load the data
     history_path = thisdir / "history.json"
@@ -13,17 +14,18 @@ def main():
 
     rows = []
     for item in data:
-        for i, rating in enumerate(item["ratings"]):
-            
-            rows.append({
-                "iteration": item["iteration"],
-                "original_prompt": item["original_prompt"],
-                "modified_prompt": item["modified_prompt"],
-                "image_path": item["image_path"],
-                "user": i,
-                "rating": rating["rating"],
-                "explanation": rating["explanation"]
-            })
+        if "ratings" in item:
+            for i, rating in enumerate(item["ratings"]):
+
+                rows.append({
+                    "iteration": item["iteration"],
+                    "original_prompt": item["original_prompt"],
+                    "modified_prompt": item["modified_prompt"],
+                    "image_path": item["image_path"],
+                    "user": i,
+                    "rating": rating["rating"],
+                    "explanation": rating["explanation"]
+                })
 
     df = pd.DataFrame(rows)
     df_mean = df.groupby(["iteration", "user"])["rating"].mean().reset_index()
@@ -31,7 +33,8 @@ def main():
     # Plot the average rating over iterations
     fig, ax = plt.subplots(figsize=(10, 6))
     for prompt, group in df_mean.groupby("user"):
-        ax.plot(group["iteration"], group["rating"], label=f"User {prompt + 1}")
+        ax.plot(group["iteration"], group["rating"],
+                label=f"User {prompt + 1}")
 
     plt.ylim(1, 5.5)
     # only draw integer ticks
@@ -44,6 +47,7 @@ def main():
     plt.legend()
     plt.grid(True)
     plt.savefig(thisdir / "ratings.png")
+
 
 if __name__ == "__main__":
     main()
